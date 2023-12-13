f416cef9201dc05ebfec995f3afaa003124d880a
Federico <vgfede@gmail.com>
2023-12-13 13:55:28 +0100

    day10 part2

--- day10/main.go
+++ day10/main.go
@@ -6,7 +6,12 @@ import (
 	"os"
 )
 
-func starting_positions(matrix [][]rune, start_i, start_j int) []rune {
+type coord struct {
+	i int
+	j int
+}
+
+func starting_directions(matrix [][]rune, start_i, start_j int) []rune {
 	var goto_pos []rune
 	// check north
 	if start_i != 0 {
@@ -61,98 +66,110 @@ func opp_dir(dir rune) rune {
 	return 'X'
 }
 
-func loop_len_dir(matrix [][]rune, start_i, start_j int, dir rune) int {
-	curr_i := start_i
-	curr_j := start_j
+func loop_len_dir(matrix [][]rune, start_i, start_j int, dir rune) (int, []coord) {
+	loop := make([]coord, 0)
+	i := start_i
+	j := start_j
 	switch dir {
 	case 'N':
-		curr_i--
+		i--
 	case 'S':
-		curr_i++
+		i++
 	case 'E':
-		curr_j++
+		j++
 	case 'W':
-		curr_i--
+		j--
 	}
-	curr_pipe := matrix[curr_i][curr_j]
+	curr_pipe := matrix[i][j]
 	dist := 1
 	coming_from := opp_dir(dir)
-	fmt.Println("Starting from", curr_i, curr_j, "coming from", string(coming_from))
 	for curr_pipe != 'S' {
-		fmt.Println("Curr pipe is", string(curr_pipe))
+		loop = append(loop, coord{i, j})
 		switch curr_pipe {
 		case '-':
-			if coming_from == 'E' && curr_j != 0 {
-				curr_j--
-			} else if coming_from == 'W' && curr_j != len(matrix[0])-1 {
-				curr_j++
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
+			if coming_from == 'E' && j != 0 {
+				j--
+			} else if coming_from == 'W' && j != len(matrix[0])-1 {
+				j++
 			} else {
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case '|':
-			if coming_from == 'N' && curr_i != len(matrix)-1 {
-				curr_i++
-			} else if coming_from == 'S' && curr_i != 0 {
-				curr_i--
+			if coming_from == 'N' && i != len(matrix)-1 {
+				i++
+			} else if coming_from == 'S' && i != 0 {
+				i--
 			} else {
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case 'F':
-			if coming_from == 'S' && curr_j != len(matrix[0])-1 {
-				curr_j++
+			if coming_from == 'S' && j != len(matrix[0])-1 {
+				j++
 				coming_from = 'W'
-			} else if coming_from == 'E' && curr_i != len(matrix)-1 {
-				curr_i++
+			} else if coming_from == 'E' && i != len(matrix)-1 {
+				i++
 				coming_from = 'N'
 			} else {
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case '7':
-			if coming_from == 'S' && curr_j != 0 {
-				curr_j--
+			if coming_from == 'S' && j != 0 {
+				j--
 				coming_from = 'E'
-			} else if coming_from == 'W' && curr_i != len(matrix)-1 {
-				curr_i++
+			} else if coming_from == 'W' && i != len(matrix)-1 {
+				i++
 				coming_from = 'N'
 			} else {
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case 'J':
-			if coming_from == 'N' && curr_j != 0 {
-				curr_j--
+			if coming_from == 'N' && j != 0 {
+				j--
 				coming_from = 'E'
-			} else if coming_from == 'W' && curr_i != 0 {
-				curr_i--
+			} else if coming_from == 'W' && i != 0 {
+				i--
 				coming_from = 'S'
 			} else {
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case 'L':
-			if coming_from == 'E' && curr_i != 0 {
-				curr_i--
+			if coming_from == 'E' && i != 0 {
+				i--
 				coming_from = 'S'
-			} else if coming_from == 'N' && curr_j != len(matrix[0])-1 {
-				curr_j++
+			} else if coming_from == 'N' && j != len(matrix[0])-1 {
+				j++
 				coming_from = 'W'
 			} else {
-				fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-				return 0
+				fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+				return 0, loop
 			}
 		case '.':
-			fmt.Println("WALL on", string(curr_pipe), "coming from", string(coming_from))
-			return 0
+			fmt.Println("Wall on", string(curr_pipe), "coming from", string(coming_from), "dist", dist)
+			return 0, loop
 		}
-		curr_pipe = matrix[curr_i][curr_j]
+		curr_pipe = matrix[i][j]
 		dist++
 	}
+	loop = append(loop, coord{i, j})
 	fmt.Println("Gone back to S with dist", dist)
 
-	return dist
+	return dist, loop
+}
+
+func print_matrix(mat [][]rune) {
+	fmt.Println("Matrix:")
+	for _, line := range mat {
+		fmt.Print("[ ")
+		for _, r := range line {
+			fmt.Print(string(r))
+		}
+		fmt.Println("]")
+	}
 }
 
 func main() {
@@ -178,18 +195,43 @@ func main() {
 		}
 		i++
 	}
-	fmt.Println("S is", start_i, start_j)
 
-	start_dir := starting_positions(matrix, start_i, start_j)
-	fmt.Println("From S start on directions", string(start_dir))
+	if len(matrix) < 32 {
+		// don't print if too big
+		print_matrix(matrix)
+	}
+	start_dir := starting_directions(matrix, start_i, start_j)
+	fmt.Println("S is at", start_i, start_j, "and can go to:", string(start_dir))
 
 	// each pipe goes on to one and only one pipe, there are no disjunctions
 	// it should be easy to do with a single loop while we don't return to S
 	maxdist := 0
+	loop := make([]coord, 0)
 	for _, dir := range start_dir {
-		maxdist = max(maxdist, loop_len_dir(matrix, start_i, start_j, dir))
+		fmt.Println("Starting on direction", string(dir))
+		dist, lp := loop_len_dir(matrix, start_i, start_j, dir)
+		if dist > maxdist {
+			maxdist = dist
+			loop = lp
+		}
 	}
-
 	fmt.Println("Result 1:", maxdist/2)
+	fmt.Println("Loop", loop)
+
+    // find enclosed with shoelace formula and pick's theorem
+	area := 0
+	for i := 0; i < len(loop); i++ {
+		curr := loop[i]
+		next := loop[(i+1)%len(loop)]
+
+		area += curr.i*next.j - curr.j*next.i
+	}
+	if area < 0 {
+		area = -area
+	}
+	area /= 2
+    enclosed := area - len(loop)/2 + 1
+
+	fmt.Println("Result 2:", enclosed)
 
 }
